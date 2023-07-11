6906219e8a51dbff108a9550529ee2259dc4b955
anandbodke0303 <anandbodke0303@gmail.com>
2023-07-11 21:29:44 +0530

    first commit

---
+++ src/main/java/project_01/project_01/test_01.java
@@ -0,0 +1,10 @@
+package project_01.project_01;
+
+public class test_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("shubham");
+	}
+
+}
